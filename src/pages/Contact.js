820d86fcc7cd52f1725b265820c8f6c12e9a8d77
CynthiaKamau <cynthiakamau54@gmail.com>
2023-02-20 21:56:04 +0300

    Added contact us section

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,102 @@
-import React from 'react'
+import React from "react";
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
+import { AiOutlineHome, AiOutlineMail } from "react-icons/ai";
+import { BiPhoneCall, BiInfoCircle } from "react-icons/bi";
+import Container from "../components/Container";
 
-export const Contact = () => {
+const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <>
+      <Meta title={"Contact Us"} />
+      <BreadCrumb title="Contact Us" />
+      <Container class1="contact-wrapper py-5 home-wrapper-2">
+        <div className="row">
+          <div className="col-12">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.817780011236!2d36.82027331394712!3d-1.2831795990640869!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f10d5d2a8bf2f%3A0x584569be8b3354cc!2sNation%20Centre%2C%20Nairobi!5e0!3m2!1sen!2ske!4v1676919244034!5m2!1sen!2ske"
+              width="600"
+              height="450"
+              className="border-0 w-100"
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+          <div className="col-12 mt-5">
+            <div className="contact-inner-wrapper d-flex justify-content-between ">
+              <div>
+                <h3 className="contact-title mb-4">Contact</h3>
+                <form action="" className="d-flex flex-column gap-15">
+                  <div>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Name"
+                    />
+                  </div>
+                  <div>
+                    <input
+                      type="email"
+                      className="form-control"
+                      placeholder="Email"
+                    />
+                  </div>
+                  <div>
+                    <input
+                      type="tel"
+                      className="form-control"
+                      placeholder="Mobile Number"
+                    />
+                  </div>
+                  <div>
+                    <textarea
+                      name=""
+                      id=""
+                      className="w-100 form-control"
+                      cols="30"
+                      rows="4"
+                      placeholder="Comments"
+                    ></textarea>
+                  </div>
+                  <div>
+                    <button className="button border-0">Submit</button>
+                  </div>
+                </form>
+              </div>
+              <div>
+                <h3 className="contact-title mb-4">Get in touch with us</h3>
+                <div>
+                  <ul className="ps-0">
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <AiOutlineHome className="fs-5" />
+                      <address className="mb-0">
+                        Nairobi, Kenya
+                      </address>
+                    </li>
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <BiPhoneCall className="fs-5" />
+                      <a href="tel:+254700000000">+254700000000</a>
+                    </li>
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <AiOutlineMail className="fs-5" />
+                      <a href="mailto:test@gmail.com">
+                        test@gmail.com
+                      </a>
+                    </li>
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <BiInfoCircle className="fs-5" />
+                      <p className="mb-0">Monday – Friday 10 AM – 8 PM</p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default Contact;
