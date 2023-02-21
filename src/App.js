54ec9d3b28993070500263b08f3a6ee89166faf9
CynthiaKamau <cynthiakamau54@gmail.com>
2023-02-21 21:43:24 +0300

    Added dummy data and refactored code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "./components/Layout";
-import { Home } from "./pages/Home";
+import Home from "./pages/Home";
 import { About } from "./pages/About";
 import Contact from "./pages/Contact";
 import OurStore from "./pages/OurStore";
